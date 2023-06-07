d8f58977fff785de24fda2838a5df9983e967f84
parshva1510 <parshva@teque7.com>
2023-06-07 12:24:22 +0530

    report

--- resources/views/admin/view_ticket.blade.php
+++ resources/views/admin/view_ticket.blade.php
@@ -251,7 +251,7 @@
 												<!--begin::Table head-->
 												<thead>
 													<!--begin::Table row-->
-													<tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+													<tr class="text-start text-gray-800 fw-bolder fs-7 text-uppercase gs-0">
 														<th class="w-10px pe-2">
 															<div class="form-check form-check-sm form-check-custom form-check-solid me-3">
 																<input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_customers_table .form-check-input" value="1" />
