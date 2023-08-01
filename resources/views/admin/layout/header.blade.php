c2b6455924b569f81cbac7259f1240ccfab025b1
parshva1510 <parshva@teque7.com>
2023-08-01 18:49:48 +0530

    header

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -192,44 +192,20 @@ License: For each use you must have a valid license purchased only from above li
 							<div class="app-navbar flex-shrink-0">
 								<!--begin::User menu-->
 								<div class="app-navbar-item ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
-									<!--begin::Menu wrapper-->
-									<div class="cursor-pointer symbol symbol-35px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-										<img src="{{url('public/assets/media/avatars/300-1.jpg')}}" alt="user" />
-									</div>
-									<!--begin::User account menu-->
-									<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
-										<!--begin::Menu item-->
-										<div class="menu-item px-3">
-											<div class="menu-content d-flex align-items-center px-3">
-												<!--begin::Avatar-->
-												<div class="symbol symbol-50px me-5">
-													<img alt="Logo" src="{{url('public/assets/media/avatars/300-1.jpg')}}" />
-												</div>
-												<!--end::Avatar-->
-												<!--begin::Username-->
-												<div class="d-flex flex-column">
-													<div class="fw-bold d-flex align-items-center fs-5">Shiv Satyam</div>
-													<span class="fw-semibold text-muted fs-7">Admin</span>
-												</div>
-												<!--end::Username-->
-											</div>
+									
+									<div style="text-align: right" class="menu-item">
+										<div class="d-flex flex-column">
+											<div class="fw-bold d-flex align-items-center fs-5">Shiv Satyam</div>
+											<span class="fw-semibold text-muted fs-7">Admin</span>
 										</div>
-										<!--end::Menu item-->
-										<!--begin::Menu separator-->
-										<div class="separator my-2"></div>
-										<!--end::Menu separator-->
-										
-										
-										
-										
-										<!--begin::Menu item-->
-										<div class="menu-item px-5">
-											<a href="{{route('destroy')}}" class="menu-link px-5">Sign Out</a>
-										</div>
-										<!--end::Menu item-->
 									</div>
-									<!--end::User account menu-->
-									<!--end::Menu wrapper-->
+									<!--<div class="cursor-pointer symbol symbol-35px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+										<img src="{{url('public/assets/media/logos/logo.png')}}" alt="user" />
+									</div>-->
+									<div class="menu-item px-4">
+											<a href="{{route('destroy')}}" class="menu-link px-2"><i style="font-size: 30px" class="fas fa-sign-out-alt"></i></a>
+									</div>
+
 								</div>
 								<!--end::User menu-->
 								<!--begin::Header menu toggle-->
